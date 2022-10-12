9d75b95f0995fe8b0924d61737fb40399f681e73
sohammondal <soham.pf.mondal@gmail.com>
2022-10-12 10:08:12 +0530

    test(App): fix failing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,17 +3,30 @@ import React from 'react'
 
 import App from './App'
 
-it('renders React Data Table heading', () => {
-  render(<App />)
-  const h1Elem = screen.getByText(/react data table/i)
-  expect(h1Elem).toBeInTheDocument()
-})
+describe('Test App component', () => {
+  beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn()
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null,
+    })
+    window.IntersectionObserver = mockIntersectionObserver
+  })
 
-it('renders DataTable with Album ID and Album Details as table headers', () => {
-  render(<App />)
-  const albumIdHeader = screen.getByText(/Album ID/i)
-  const albumDetailsHeader = screen.getByText(/Album Details/i)
+  it('renders React Data Table heading', () => {
+    render(<App />)
+    const h1Elem = screen.getByText(/react data table/i)
+    expect(h1Elem).toBeInTheDocument()
+  })
 
-  expect(albumIdHeader).toBeInTheDocument()
-  expect(albumDetailsHeader).toBeInTheDocument()
+  it('renders DataTable with Album ID and Album Details as table headers', () => {
+    render(<App />)
+    const albumIdHeader = screen.getByText(/Album ID/i)
+    const albumDetailsHeader = screen.getByText(/Album Details/i)
+
+    expect(albumIdHeader).toBeInTheDocument()
+    expect(albumDetailsHeader).toBeInTheDocument()
+  })
 })
